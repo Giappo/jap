4b12364668243c10caa87c359cdf494e2e667cb9
Giappo <glaudanno@gmail.com>
2019-11-28 12:27:16 +0100

    reorganizing functions

--- R/rlibrary_functions.R
+++ R/rlibrary_functions.R
@@ -26,31 +26,6 @@ install_richel <- function() {
   }
 }
 
-#' @title cat2
-#' @author Giovanni Laudanno
-#' @description If verbose == TRUE cats the message, otherwise stays silent
-#' @inheritParams default_params_doc
-#' @return prints on screen
-#' @export
-cat2 <- function(
-  message,
-  verbose
-) {
-  if (verbose == TRUE) {
-    cat(message)
-  } else {
-    return()
-  }
-}
-
-#' Like file.path, but cooler
-#' @param fsep path separator for the OS
-#' @param ... additional arguments
-#' @export
-file_path <- function(..., fsep = .Platform$file.sep) {
-  gsub("//", "/", file.path(..., fsep = fsep))
-}
-
 #' Open R library
 #' @export
 open_rlibrary <- function() {
