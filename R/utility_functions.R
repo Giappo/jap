367aa427c32ff8b96d4b18e0648652a99c6d1449
Giappo <glaudanno@gmail.com>
2019-12-16 12:12:53 +0100

    added plot_matrix

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -43,3 +43,30 @@ my_try_catch <- function(expr) {
     error = err
   )
 }
+
+#' Just plot a matrix without rotating it
+#' @param logs do you want to plot in log scale?
+#' @param low_triangular do you want to plot only the low triangular?
+#' @export
+plot_matrix <- function(
+  mat,
+  logs = TRUE,
+  low_triangular = FALSE
+) {
+  if (low_triangular == TRUE) {
+    mat[col(mat) >= row(mat)] <- 0
+  }
+  rotate <- function(x) t(apply(x, 2, rev))
+  col_palette <- colorRampPalette(
+    c('blue', 'white', 'red')
+  )(30)
+  if (logs == TRUE) {
+    mat2 <- log(mat)
+  } else {
+    mat2 <- mat
+  }
+  levelplot(
+    rotate(mat2),
+    col.regions = col_palette
+  )
+}
