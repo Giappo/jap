3ba6f1b89dbd0ec36b81d4a2ced6470230235a66
Giappo <glaudanno@gmail.com>
2020-02-08 21:17:53 +0100

    bash for pirouette thx @richelbilderbeek

---
+++ cluster_scripts/run_pir_example_gl.bash
@@ -0,0 +1,8 @@
+example_no=$1
+
+module load git
+git clone https://github.com/richelbilderbeek/peregrine.git
+git clone https://github.com/richelbilderbeek/pirouette_example_${example_no}.git
+cd pirouette_example_${example_no}/
+module load R
+sbatch ../peregrine/scripts/run_r_script.sh example_${example_no}_gl.R 
