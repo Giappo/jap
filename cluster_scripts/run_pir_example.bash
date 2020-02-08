43781639beb5f408368d5cfc0023b91884b0753f
Giappo <glaudanno@gmail.com>
2020-02-08 22:19:17 +0100

    install pirouette via peregrine

--- cluster_scripts/run_pir_example.bash
+++ cluster_scripts/run_pir_example.bash
@@ -5,7 +5,8 @@ example_no=$1
 
 module load git
 git clone https://github.com/richelbilderbeek/peregrine.git
+sbatch ../peregrine/scripts/install_pirouette.sh
 git clone https://github.com/richelbilderbeek/pirouette_example_${example_no}.git
 cd pirouette_example_${example_no}/
 module load R
-sbatch ../peregrine/scripts/run_r_script.sh example_${example_no}.R 
+sbatch ../peregrine/scripts/run_r_script.sh example_${example_no}.R
