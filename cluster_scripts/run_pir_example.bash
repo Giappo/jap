9964292bbe125adb50b0ffe7a8c13aba74eee2a9
Giappo <glaudanno@gmail.com>
2020-02-09 16:27:47 +0100

    pippo

--- cluster_scripts/run_pir_example.bash
+++ cluster_scripts/run_pir_example.bash
@@ -4,10 +4,10 @@
 example_no=$1
 
 module load git
+module load R
 git clone https://github.com/richelbilderbeek/peregrine.git
 sbatch ./peregrine/scripts/install_beast2.sh
 sbatch ./peregrine/scripts/install_pirouette.sh
 git clone https://github.com/richelbilderbeek/pirouette_example_${example_no}.git
 cd pirouette_example_${example_no}/
-module load R
 sbatch ../peregrine/scripts/run_r_script.sh example_${example_no}.R
