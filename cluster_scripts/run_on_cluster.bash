e29edd7ceba9ff79dc6f33537f4bdf1345962c70
Giappo <glaudanno@gmail.com>
2020-02-13 00:04:06 +0100

    pippobaudo3

--- cluster_scripts/run_on_cluster.bash
+++ cluster_scripts/run_on_cluster.bash
@@ -35,6 +35,10 @@ rm $R_file_name #remove previous versions
 rm $bash_file_name #remove previous versions
 
 echo "args <- commandArgs(TRUE)" > $R_file_name
+echo "print(args[1])" >> $R_file_name
+echo "print(args[2])" >> $R_file_name
+echo "print(args[3])" >> $R_file_name
+echo "print(args[4])" >> $R_file_name
 echo "x <- jap::run_function(github_name = args[1], package_name = args[2], function_name = args[3], fun_arguments = args[4])" >> $R_file_name
 echo "print(x)" >> $R_file_name
 echo 'save(x, file = file.path(getwd(), \"out.RData\"))' >> $R_file_name
@@ -43,7 +47,7 @@ echo "#!/bin/bash" > $bash_file_name
 echo "#SBATCH --time=71:58:58" >> $bash_file_name
 echo "#SBATCH --output=bash-${funname}.log" >> $bash_file_name
 echo "module load R" >> $bash_file_name
-echo "Rscript -e ${R_file_name} ${args_vector[@]}" >> $bash_file_name
+echo "Rscript ${R_file_name} ${args_vector[@]}" >> $bash_file_name
 echo "rm ${R_file_name}" >> $bash_file_name
 echo "rm ${bash_file_name}" >> $bash_file_name
 
