1f54bb5ff6f3af122e0a51c59a849740e916f065
Giappo <glaudanno@gmail.com>
2020-02-13 23:30:38 +0100

    pb7

--- cluster_scripts/run_on_cluster.bash
+++ cluster_scripts/run_on_cluster.bash
@@ -19,6 +19,7 @@ R_file_name=R-${args_string}.R
 bash_file_name=bash-${args_string}.bash
 job_name=${args_string}
 log_name=${args_string}.log
+out_name=${args_string}.RData
 
 rm $R_file_name #remove previous versions
 rm $bash_file_name #remove previous versions
@@ -28,7 +29,7 @@ echo "print(args)" >> $R_file_name
 echo "load(file.path(getwd(), \"${fun_file}\"))" >> $R_file_name
 echo "x <- fun_list\$run_function_from_file(args_file = args)" >> $R_file_name
 echo "print(x)" >> $R_file_name
-#echo 'save(x, file = file.path(getwd(), \"out.RData\"))' >> $R_file_name
+echo 'save(x, file = file.path(getwd(), \"${out_name}\"))' >> $R_file_name
 
 echo "#!/bin/bash" > $bash_file_name
 echo "#SBATCH --time=71:58:58" >> $bash_file_name
@@ -37,6 +38,8 @@ echo "module load R" >> $bash_file_name
 echo "Rscript ${R_file_name} ${args_file}" >> $bash_file_name
 echo "rm ${R_file_name}" >> $bash_file_name
 echo "rm ${bash_file_name}" >> $bash_file_name
+echo "rm ${args_file}" >> $bash_file_name
+echo "rm ${fun_file}" >> $bash_file_name
 
 #NEVER ASK FOR MORE THAN 9GB OF MEMORY!
 sbatch  --partition=$chosen_partition \
