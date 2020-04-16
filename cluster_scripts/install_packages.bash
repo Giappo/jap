fe083183fd482cd8eb87b6a13093ee42dfff4197
Giappo <glaudanno@gmail.com>
2020-04-16 13:53:07 +0200

    remote_install.packages 2

--- cluster_scripts/install_packages.bash
+++ cluster_scripts/install_packages.bash
@@ -13,7 +13,5 @@ cd /home/$USER/
 #chmod +x /home/$USER/Rlibrary/
 
 module load R
-#Rscript -e "install.packages(\"$github_address\")"
-#Rscript -e "devtools::install_github(\"$github_address\")"
-Rscript -e "ifelse(grepl(pattern = \"/\", x = \"$pkg_name\"), yes = devtools::install_github(\"$pkg_name\"), no = install.packages(\"$pkg_name\", repos = \"https://lib.ugent.be/CRAN/\"))"
+Rscript -e "if (grepl(pattern = \"/\", x = \"$pkg_name\")) {devtools::install_github(\"$pkg_name\")} else {install.packages(\"$pkg_name\", repos = \"https://lib.ugent.be/CRAN/\")}"
 #Rscript -e ".libPaths(new = file.path(substring(getwd(),1,13), 'Rlibrary')); devtools::install_github(\"$github_address\")"
